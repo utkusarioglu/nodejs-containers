f008254003cd0da0b2c73a03156fd005f76c285b
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-12-11 10:35:15 +0000

    New

--- src/home/scripts/bootstrap/nodejs.sh
+++ src/home/scripts/bootstrap/nodejs.sh
@@ -6,8 +6,10 @@ bash --version
 apt-get update 
 apt-get upgrade -y
 apt-get install -y software-properties-common 
-wget -O nodesource_setup.sh https://deb.nodesource.com/setup_23.x
-./nodesource_setup.sh
+node_file=nodesource_setup.sh
+wget -O ${node_file} https://deb.nodesource.com/setup_23.x
+chmod +x ${node_file}
+./${node_file}
 # curl -sL https://deb.nodesource.com/setup_220.x | bash - 
 
 apt-get install -y nodejs
